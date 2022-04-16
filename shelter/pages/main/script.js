a507c38c0052351dae986f45bd5649db64921ec8
Olga <olga.dashuk21@gmail.com>
2022-04-15 20:46:26 -0400

    feat: add burger menu to mobile (main page)

---
+++ shelter/pages/main/script.js
@@ -0,0 +1,26 @@
+"use strict"
+const iconBurger = document.querySelector('.header__burger');
+const menuBurger = document.querySelector('.header__nav');
+const logoRegular = document.querySelector('.header__logo');
+const logoBurger = document.querySelector('.header__burger-logo');
+const headerWrapper = document.querySelector('.header__wrapper');
+
+iconBurger.addEventListener('click', function(e){
+    menuBurger.classList.toggle('_active');
+    iconBurger.classList.toggle('_active');
+    logoRegular.classList.toggle('_active');
+    logoBurger.classList.toggle('_active');
+    headerWrapper.classList.toggle('_active');
+    document.body.classList.toggle('_lock');
+});
+
+const menuLinks = document.querySelectorAll('.header__link');
+menuLinks.forEach(menuLink => menuLink.addEventListener('click', closeMenu));
+function closeMenu () {
+    menuBurger.classList.remove('_active');
+    iconBurger.classList.remove('_active');
+    logoRegular.classList.remove('_active');
+    logoBurger.classList.remove('_active');
+    headerWrapper.classList.remove('_active');
+    document.body.classList.remove('_lock');
+}
